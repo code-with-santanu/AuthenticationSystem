4f882c55eb0d602e61ed0419248cd83883cf7e57
code-with-santanu <santanupal003@gmail.com>
2024-04-04 00:19:37 +0530

    src/
    initial setup

---
+++ src/test/java/com/santanu/gossipZ/GossipZApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santanu.gossipZ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GossipZApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
