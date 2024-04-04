74eb465425772c3cf1de5c3644777f205b1a77b9
code-with-santanu <santanupal003@gmail.com>
2024-04-04 21:26:48 +0530

    userservice implemented

---
+++ src/main/java/com/santanu/gossipZ/service/UserServiceImpl.java
@@ -0,0 +1,98 @@
+package com.santanu.gossipZ.service;
+
+import com.santanu.gossipZ.entity.UserEntity;
+import com.santanu.gossipZ.model.User;
+import com.santanu.gossipZ.repository.UserRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+   private UserRepository userRepository;
+
+   @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public List<User> getAllUser() {
+        // retrieve the list of user-entity
+        List<UserEntity> userEntityList = userRepository.findAll();
+
+        // convert the user-entity list to a user list
+        List<User> userList = userEntityList
+                .stream()
+                .map(userEntity -> new User(
+                        userEntity.getId(),
+                        userEntity.getFirstName(),
+                        userEntity.getLastName(),
+                        userEntity.getEmail()
+                )).collect(Collectors.toList());
+
+        return userList;
+    }
+
+    @Override
+    public User getUserById(Long userId) {
+        UserEntity userEntity = userRepository.findById(userId).get(); // without get() method , we will get the instance only
+
+        //convert the user-entity to user model data
+        User user = new User();
+        BeanUtils.copyProperties(userEntity,user);
+
+        return user;
+    }
+
+    @Override
+    @Transactional
+    public User addUser(User newUser) {
+
+        // create a user-entity object
+        UserEntity userEntity = new UserEntity();
+
+        // copy the User properties to the user-entity
+        BeanUtils.copyProperties(newUser,userEntity);
+
+        //In case they pass an id in JSON... set id to 0; this is to force a save of new item instead of update
+        userEntity.setId(0);
+
+        // save the new user using repository and user-entity
+        userEntity=userRepository.save(userEntity);
+
+        BeanUtils.copyProperties(userEntity,newUser);
+
+        return newUser;
+    }
+
+
+    @Override
+    @Transactional
+    public User updateUser(Long userId,User newUser)
+    {
+        UserEntity userEntity = userRepository.findById(userId).get();
+
+        userRepository.save(userEntity);
+        return newUser;
+    }
+
+
+    @Override
+    @Transactional
+    public boolean deleteUser(Long userId) {
+
+       UserEntity theUser = userRepository.findById(userId).get();
+
+       if(theUser==null)
+           throw new RuntimeException("User not found ---> "+userId);
+        userRepository.deleteById(userId);
+
+        return true;
+    }
+}
