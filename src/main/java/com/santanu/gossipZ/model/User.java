fe9bf9d9fcfa4dd4050dd7c5d8406393692614d4
code-with-santanu <santanupal003@gmail.com>
2024-04-04 17:03:56 +0530

    user model created

---
+++ src/main/java/com/santanu/gossipZ/model/User.java
@@ -0,0 +1,63 @@
+package com.santanu.gossipZ.model;
+
+import jakarta.persistence.Column;
+
+public class User {
+
+    // ***** Defining fields *****
+    private long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+
+    // ***** Defining constructors *****
+
+    public User() {
+    }
+
+    public User(long id, String firstName, String lastName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+
+    // ***** Defining getters and setters *****
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
