e6bd54a9199ce5b9f89a98a3bc7b938baab8354d
code-with-santanu <santanupal003@gmail.com>
2024-05-01 23:31:19 +0530

    file added

---
+++ src/main/java/com/santanu/chatlynk/model/AuthenticationResponse.java
@@ -0,0 +1,13 @@
+package com.santanu.chatlynk.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class AuthenticationResponse {
+
+    private String token;
+
+
+}
