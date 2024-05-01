3b29e734f4bb8844c3fcd365b9c9306d0009c2cf
code-with-santanu <santanupal003@gmail.com>
2024-05-02 03:18:45 +0530

    loginsuccess handler added

---
+++ src/main/java/com/santanu/chatlynk/config/LoginSuccessHandler.java
@@ -0,0 +1,24 @@
+package com.santanu.chatlynk.config;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class LoginSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
+    @Value("${CLIENT_URL}")
+    private String redirect_url;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws ServletException, IOException {
+        this.setAlwaysUseDefaultTargetUrl(true);
+        this.setDefaultTargetUrl(redirect_url);
+        super.onAuthenticationSuccess(request, response, authentication);
+    }
+}
