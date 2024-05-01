e5cce97942fc477fb0e071873db4494696ef05b2
code-with-santanu <santanupal003@gmail.com>
2024-05-01 23:30:40 +0530

    entity for jwt auth implemented

---
+++ src/main/java/com/santanu/chatlynk/entity/Role.java
@@ -0,0 +1,6 @@
+package com.santanu.chatlynk.entity;
+
+public enum Role {
+    ADMIN,
+    USER
+}
---
+++ src/main/java/com/santanu/chatlynk/entity/User.java
@@ -0,0 +1,70 @@
+package com.santanu.chatlynk.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="users")
+@Entity
+public class User implements UserDetails {
+
+    // Fields of entity
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private long id;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Enumerated(value = EnumType.STRING)
+    private Role role;
+
+
+    // Overridden methods
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
