7302aec8495516feca9635e37bcb17ad9158b1c1
code-with-santanu <santanupal003@gmail.com>
2024-05-06 22:01:07 +0530

    script updated

--- sql_database_scripts/chatlync_directory.sql
+++ sql_database_scripts/chatlync_directory.sql
@@ -5,19 +5,38 @@ USE `chatlync_directory`;
 -- Table structure for table `users`
 --
 
+
 DROP TABLE IF EXISTS `users`;
 
+
 CREATE TABLE `users` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` bigint NOT NULL AUTO_INCREMENT,
   `first_name` varchar(45) DEFAULT NULL,
   `last_name` varchar(45) DEFAULT NULL,
   `email` varchar(50) DEFAULT NULL,
   `password` varchar(100) NOT NULL,
   `role` enum ('USER','ADMIN'),
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`) 
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
 
+--
+-- Table structure for table `token`
+--
+
+
+DROP TABLE IF EXISTS `token`;
+
+
+create table `token` (
+	`id` bigint not null auto_increment,
+	`logged_out` bit DEFAULT NULL,
+    `logged_out_time` datetime DEFAULT NULL,
+    `token` varchar(255),
+    `user_id` bigint,
+    primary key (id)) engine=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+alter table `token` add constraint fk_user foreign key (`user_id`) references `users` (id);
 
 -- INSERT INTO `users` VALUES 
 -- 	(1,'Leslie','Andrews','leslie@luv2code.com','12345','123'),
