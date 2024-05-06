989a8a432744c6c0aba07512eeca932e18bac2f3
code-with-santanu <santanupal003@gmail.com>
2024-05-07 00:03:03 +0530

    user esistence for registration handled and logout functinality added

--- src/main/java/com/santanu/chatlynk/service/JwtService.java
+++ src/main/java/com/santanu/chatlynk/service/JwtService.java
@@ -1,10 +1,12 @@
 package com.santanu.chatlynk.service;
 
 import com.santanu.chatlynk.entity.User;
+import com.santanu.chatlynk.repository.TokenRepository;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 
 import org.springframework.security.core.userdetails.UserDetails;
@@ -14,13 +16,19 @@ import javax.crypto.SecretKey;
 import java.util.Date;
 import java.util.function.Function;
 
+@RequiredArgsConstructor
 @Service
 public class JwtService {
     @Value("${security.jwt.secret-key}")
     private String secretKey;
 
-    @Value("${security.jwt.expiration-time}")
-    private long jwtExpiration;
+    @Value("${security.jwt.access-token-expiration-time}")
+    private long jwtAccessTokenExpiration;
+
+    @Value("${security.jwt.access-token-expiration-time}")
+    private long jwtRefreshTokenExpiration;
+
+    private final TokenRepository tokenRepository;
 
     // Extract all-claims from token i.e, subject,issuedAt, expiry,signInKey
     private Claims extractAllClaims(String token) {
@@ -46,7 +54,13 @@ public class JwtService {
     // Check the token validity
     public boolean isTokenValid(String token, UserDetails user) {
         final String username = extractUsername(token);
-        return (username.equals(user.getUsername()) && !isTokenExpired(token));
+
+        // check if the current token is logged out or not
+        boolean isLoggedOut = tokenRepository.findByToken(token)
+                .map(t -> !t.isLoggedOut()).orElse(false);
+
+        // return true if (1)username is correct , (2)token is not expired and (3)token is not logged out
+        return (username.equals(user.getUsername()) && !isTokenExpired(token) && isLoggedOut);
     }
 
     // Extract the specific claim(expiration time)
@@ -60,13 +74,23 @@ public class JwtService {
     }
 
 
+    // method for generating token of validation 24hrs
+    public String generateAccessToken(User user){
+        return generateToken(user,jwtAccessTokenExpiration);
+    }
+
+    // method for generating token of validation 7days
+    public String generateRefreshToken(User user){
+        return generateToken(user,jwtRefreshTokenExpiration);
+    }
+
     // Generating jwt token
-    public String generateToken(User user) {
+    public String generateToken(User user,long expirationTime) {
         String token = Jwts
                 .builder()
                 .setSubject(user.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + jwtExpiration))
+                .setExpiration(new Date(System.currentTimeMillis() + expirationTime))
                 .signWith(getSignInKey())
                 .compact();
 
