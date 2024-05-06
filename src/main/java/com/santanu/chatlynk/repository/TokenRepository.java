8b57bdfc05762460aaf8599d4a8426bc9f67b129
code-with-santanu <santanupal003@gmail.com>
2024-05-06 21:42:42 +0530

    repository added to retrieve token info

---
+++ src/main/java/com/santanu/chatlynk/repository/TokenRepository.java
@@ -0,0 +1,20 @@
+package com.santanu.chatlynk.repository;
+
+import com.santanu.chatlynk.entity.Token;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TokenRepository extends JpaRepository<Token,Long> {
+
+    @Query("""
+            Select t from Token t inner join User u
+            on t.user.id = u.id
+            where t.user.id =:userId and t.loggedOut = false
+            """)
+    List<Token> findAllTokenByUser(Long userId);
+
+    Optional<Token> findByToken(String token);
+}
