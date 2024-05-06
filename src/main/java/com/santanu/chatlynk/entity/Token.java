89cca29742ccef28f21bd30ca35b46809e6da6f6
code-with-santanu <santanupal003@gmail.com>
2024-05-06 21:56:28 +0530

    new Token entity added

---
+++ src/main/java/com/santanu/chatlynk/entity/Token.java
@@ -0,0 +1,36 @@
+package com.santanu.chatlynk.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@Table(name="token")
+@Entity
+public class Token {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "token")
+    private String token;
+
+    @Column(name = "logged_out")
+    private boolean loggedOut;
+
+    @Column(name = "logged_out_time")
+    private Date loggedOutTime;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+
+
+
+}
