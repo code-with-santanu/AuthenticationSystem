ebf974de8b1f365f4ed146a52528b8d5c6e195b2
code-with-santanu <santanupal003@gmail.com>
2024-05-07 00:47:30 +0530

    exception handler for jwtFilter added

---
+++ src/main/java/com/santanu/chatlynk/config/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,50 @@
+package com.santanu.chatlynk.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.Collections;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+//    This class is used to return a 401 unauthorized error to clients that try to access a protected resource without proper authentication.
+
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        String message;
+        // Check if the request as any exception that we have stored in Request
+        final Exception exception = (Exception) request.getAttribute("exception");
+
+        // If yes then use it to create the response message else use the authException
+        if (exception != null) {
+
+            byte[] body = new ObjectMapper().writeValueAsBytes(Collections.singletonMap("cause", exception.toString()));
+            response.getOutputStream().write(body);
+        }else {
+            if (authException.getCause() != null) {
+                message = authException.getCause().toString() + " " + authException.getMessage();
+            } else {
+                message = authException.getMessage();
+            }
+
+            byte[] body = new ObjectMapper().writeValueAsBytes(Collections.singletonMap("error", message));
+
+            response.getOutputStream().write(body);
+        }
+    }
+
+
+}
