858e8ca9f81578f22ebf983353f3b76a6d8dd2d1
code-with-santanu <santanupal003@gmail.com>
2024-07-12 03:20:05 +0530

    Data validation fixed

--- src/main/java/com/santanu/jwtAuth/model/AuthenticationRequest.java
+++ src/main/java/com/santanu/jwtAuth/model/AuthenticationRequest.java
@@ -1,5 +1,9 @@
 package com.santanu.jwtAuth.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NonNull;
@@ -10,9 +14,25 @@ import lombok.Setter;
 @Data
 public class AuthenticationRequest {
 
-    @NonNull
-    private String email;
+    @NotEmpty(message = "Username can't be null or empty")
+    @Email(message = "Enter a correct email address")
+    private String username;
 
-    @NonNull
+    @NotEmpty(message = "Password can't be null or empty")
+    @Size(max = 15, message = "Password should be maximum 15 character long")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$" , message = "This violets the password pattern")
     private String password;
 }
+
+
+// Explaination of above password pattern regex:
+/*
+    ^                 # start-of-string
+    (?=.*[0-9])       # a digit must occur at least once
+    (?=.*[a-z])       # a lower case letter must occur at least once
+    (?=.*[A-Z])       # an upper case letter must occur at least once
+    (?=.*[@#$%^&+=])  # a special character must occur at least once
+    (?=\S+$)          # no whitespace allowed in the entire string
+    .{8,}             # anything, at least eight places though
+    $                 # end-of-string
+ */
