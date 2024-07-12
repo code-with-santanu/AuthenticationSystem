db881cdd60eeea64cb57f2aa114e13b7304d551d
code-with-santanu <santanupal003@gmail.com>
2024-07-12 19:19:43 +0530

    updated

--- src/main/java/com/santanu/jwtAuth/config/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/santanu/jwtAuth/config/JwtAuthenticationEntryPoint.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
@@ -12,27 +14,33 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 import java.util.Collections;
 
+/* Custom Handler for unauthorized request
+   i.e, This class is used to return a 401 unauthorized error to clients that try to access a protected resource without proper authentication. */
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
-//    This class is used to return a 401 unauthorized error to clients that try to access a protected resource without proper authentication.
-
+    private static Logger logger = LoggerFactory.getLogger(AuthenticationEntryPoint.class);
     @Override
     public void commence(HttpServletRequest request,
                          HttpServletResponse response,
                          AuthenticationException authException) throws IOException, ServletException {
+
+        // Log the exception
+        logger.error("Unauthorized error: {}",authException.getMessage());
+
+        // Step 1: Set the response
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
 
         String message;
-        // Check if the request as any exception that we have stored in Request
+        // Step 2: Check if the request has any exception that we have stored in Request from the auth-filter
         final Exception exception = (Exception) request.getAttribute("exception");
 
-        // If yes then use it to create the response message else use the authException
+        // Step 2.1: If yes then use it to create the response message else use the authException
         if (exception != null) {
 
             byte[] body = new ObjectMapper().writeValueAsBytes(Collections.singletonMap("cause", exception.toString()));
-            response.getOutputStream().write(body);
+            response.getOutputStream().write(body); // write the response body and return to the client
         }else {
             if (authException.getCause() != null) {
                 message = authException.getCause().toString() + " " + authException.getMessage();
@@ -41,8 +49,7 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
             }
 
             byte[] body = new ObjectMapper().writeValueAsBytes(Collections.singletonMap("error", message));
-
-            response.getOutputStream().write(body);
+            response.getOutputStream().write(body); // write the response body and return to the client
         }
     }
 
