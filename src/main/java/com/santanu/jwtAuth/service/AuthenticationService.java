c10e166449e5e89eff969ff7708e38448b915fd9
code-with-santanu <santanupal003@gmail.com>
2024-07-17 03:30:29 +0530

    reset-password func added

--- src/main/java/com/santanu/jwtAuth/service/AuthenticationService.java
+++ src/main/java/com/santanu/jwtAuth/service/AuthenticationService.java
@@ -2,19 +2,20 @@ package com.santanu.jwtAuth.service;
 
 import com.santanu.jwtAuth.entity.Token;
 import com.santanu.jwtAuth.entity.User;
-import com.santanu.jwtAuth.model.AuthenticationResponse;
-import com.santanu.jwtAuth.model.AuthenticationRequest;
-import com.santanu.jwtAuth.model.RegisterResponse;
-import com.santanu.jwtAuth.model.RegisterUser;
+import com.santanu.jwtAuth.model.*;
 import com.santanu.jwtAuth.repository.TokenRepository;
 import com.santanu.jwtAuth.repository.UserRepository;
 import com.santanu.jwtAuth.utils.JwtUtils;
 import lombok.AllArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.nio.file.AccessDeniedException;
+import java.security.Principal;
 import java.util.Date;
 import java.util.List;
 
@@ -93,5 +94,74 @@ public class AuthenticationService {
         token.setLoggedOut(false);
         token.setUser(user);
         tokenRepository.save(token);
+    }
+
+    public RegisterResponse updatePassword(PasswordResetRequest request, Authentication authentication) {
+
+        try{
+            String username = getLoggedInUsername(authentication);
+            System.out.println("Current logged in username: "+ username);
+            String curPassword = getLoggedInPassword(authentication);
+            System.out.println("Password received...");
+
+            // Check if username present and given old-password and stored password matches
+            if(username!=null && isPasswordMatch(request.getOldPassword(), curPassword) ){
+                // Find the user
+                User existUser = userRepository.findByUsername(username).orElse(new User());
+                // Update the password
+                existUser.setPassword(passwordEncoder.encode(request.getNewPassword()));
+                // Save the updated user
+                userRepository.save(existUser);
+                // Forcibly log out all the session the current user
+                revokeAllTokenByUser(existUser);
+
+
+                return (new RegisterResponse(200,"Password is updated Successfully"));
+            }
+            else {
+                throw new RuntimeException("Couldn't update the password");
+            }
+        }catch (Exception ex){
+
+            return (new RegisterResponse(500,"Couldn't update the password"));
+        }
+    }
+
+    // Retrieve username of current logged-in user
+    public String getLoggedInUsername(Authentication authentication) throws AccessDeniedException {
+        if (authentication != null && authentication.isAuthenticated()) {
+            Object principal = authentication.getPrincipal();
+            if (principal instanceof User) {
+                return ((User) principal).getUsername();
+            } else {
+                return principal.toString();
+                // throw authentication not found error
+            }
+        }
+        else {
+            throw new AccessDeniedException("Unauthenticated user");
+        }
+    }
+
+    // Retrieve password of current logged-in user
+    public String getLoggedInPassword(Authentication authentication) throws AccessDeniedException {
+        if (authentication != null && authentication.isAuthenticated()) {
+            Object principal = authentication.getPrincipal();
+            if (principal instanceof User) {
+                return ((User) principal).getPassword();
+            } else {
+                return principal.toString();
+                // throw authentication not found error
+            }
+        }
+        else {
+            throw new AccessDeniedException("Unauthenticated user");
+        }
+
+    }
+
+    // check given password and stored password is same or not
+    private boolean isPasswordMatch(String rawPassword, String encodedPassword){
+        return passwordEncoder.matches(rawPassword,encodedPassword);
     }
 }
