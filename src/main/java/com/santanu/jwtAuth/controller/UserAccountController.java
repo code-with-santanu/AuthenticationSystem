1db55a93197fb9f165a4458f868fbf92dd57c8c9
code-with-santanu <santanupal003@gmail.com>
2024-07-21 03:15:38 +0530

    endpoint added

--- src/main/java/com/santanu/jwtAuth/controller/UserAccountController.java
+++ src/main/java/com/santanu/jwtAuth/controller/UserAccountController.java
@@ -1,27 +1,31 @@
 package com.santanu.jwtAuth.controller;
 
+import com.santanu.jwtAuth.entity.User;
 import com.santanu.jwtAuth.model.PasswordResetRequest;
 import com.santanu.jwtAuth.model.RegisterResponse;
-import com.santanu.jwtAuth.service.AuthenticationService;
+import com.santanu.jwtAuth.service.AuthenticationServiceImpl;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/account")
 public class UserAccountController {
 
-    private final AuthenticationService authenticationService;
+    private final AuthenticationServiceImpl authenticationServiceImpl;
 
     // Expose endpoint to reset password
     @PutMapping("/resetPassword")
     public ResponseEntity<RegisterResponse> resetPassword(@Valid @RequestBody PasswordResetRequest request, Authentication authentication) {
-        return ResponseEntity.ok(authenticationService.updatePassword(request,authentication));
+        return ResponseEntity.ok(authenticationServiceImpl.updatePassword(request,authentication));
+    }
+
+    // Expose endpoint to fetch current-user info
+    @GetMapping("/getUserInfo")
+    public ResponseEntity<User> getUserInfo(Authentication authentication) {
+        return ResponseEntity.ok(authenticationServiceImpl.getLoggedInSUserInfo(authentication));
     }
 }
