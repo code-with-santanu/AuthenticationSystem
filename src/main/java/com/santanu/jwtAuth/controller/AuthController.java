0e4c72bd68b5bdb86a2c0a1c9e51b78112722b99
code-with-santanu <santanupal003@gmail.com>
2024-07-21 03:17:04 +0530

    updated

--- src/main/java/com/santanu/jwtAuth/controller/AuthController.java
+++ src/main/java/com/santanu/jwtAuth/controller/AuthController.java
@@ -1,7 +1,7 @@
 package com.santanu.jwtAuth.controller;
 
 import com.santanu.jwtAuth.model.*;
-import com.santanu.jwtAuth.service.AuthenticationService;
+import com.santanu.jwtAuth.service.AuthenticationServiceImpl;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -13,12 +13,12 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/auth")
 public class AuthController {
 
-    private final AuthenticationService authenticationService;
+    private final AuthenticationServiceImpl authenticationServiceImpl;
 
     @PostMapping("/register")
     public ResponseEntity<RegisterResponse> register(@Valid @RequestBody RegisterUser request){
         // Get the response
-        RegisterResponse response = authenticationService.register(request);
+        RegisterResponse response = authenticationServiceImpl.register(request);
 
         // if user exists send status code 400 else status code 200
         if(response.getStatusCode()==400)
@@ -28,6 +28,6 @@ public class AuthController {
 
     @PostMapping("/login")
     public ResponseEntity<AuthenticationResponse> login(@Valid @RequestBody AuthenticationRequest request){
-        return ResponseEntity.ok(authenticationService.authenticate(request));
+        return ResponseEntity.ok(authenticationServiceImpl.authenticate(request));
     }
 }
